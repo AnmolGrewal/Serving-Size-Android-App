884577d51a35c873eaf52769af81d834e4900ae9
Anmo <anmolsgrewal@gmail.com>
2018-01-29 21:14:30 -0800

    3.4 Started + Code Coverage 100%

---
+++ app/src/main/java/assignment1/anmol/servingsizecalculator/Servings.java
@@ -0,0 +1,13 @@
+package assignment1.anmol.servingsizecalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Servings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servings);
+    }
+}
